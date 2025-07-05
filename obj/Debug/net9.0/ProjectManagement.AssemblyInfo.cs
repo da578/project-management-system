bb5e961350ec260425fd724d864429046c8242a0
Dzulkifli Anwar <dzulkiflianwar2@gmail.com>
2025-07-06 06:59:03 +0700

    ignore obj folder

--- obj/Debug/net9.0/ProjectManagement.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProjectManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2404284659419eed73ae44ef67bc281892f07bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
